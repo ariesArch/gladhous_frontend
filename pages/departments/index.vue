86ec1cee6c8fec043fce3eaa497845c08a5455ef
SuMonAye216827 <sumon.sue@gmail.com>
2020-12-21 20:21:27 +0630

    Stetup Deparments and Roles

--- pages/departments/index.vue
+++ pages/departments/index.vue
@@ -1,8 +1,53 @@
 <template>
   <v-row>
     <v-col>
-      <v-btn @click="onCreate">New Department</v-btn>
       <DepartmentForm />
+      <client-only>
+        <v-data-table
+          :headers="headers"
+          :items="departments"
+          :search="search"
+        >
+          <template v-slot:top>
+            <v-toolbar
+              flat
+            >
+              <v-toolbar-title>Departments List</v-toolbar-title>
+              <v-spacer />
+              <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Search in Departments"
+                hide-details
+              />
+              <v-spacer />
+              <v-btn
+                color="primary"
+                dark
+                class="mb-4"
+                @click="onCreate"
+              >
+                New Department
+              </v-btn>
+            </v-toolbar>
+          </template>
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon
+
+              class="mr-2"
+              @click="onEdit(item)"
+            >
+              mdi-pencil
+            </v-icon>
+            <v-icon
+              color="warning"
+              @click="deleteItem(item)"
+            >
+              mdi-delete
+            </v-icon>
+          </template>
+        </v-data-table>
+      </client-only>
     </v-col>
   </v-row>
 </template>
@@ -12,9 +57,30 @@ export default {
   components: {
     DepartmentForm
   },
+  async asyncData ({ $content, params }) {
+    const departments = await $content('departments', params.slug)
+      .sortBy('createdAt', 'desc')
+      .fetch();
+
+    return { departments };
+  },
+  data: () => {
+    return {
+      search: '',
+      headers: [
+        { text: 'Name', value: 'name_mm' },
+        { text: 'Name_En', value: 'name_en' },
+        { text: 'Description', value: 'description' },
+        { text: 'Actions', value: 'actions', sortable: false }
+      ]
+    };
+  },
   methods: {
     onCreate () {
       this.$emit('createForm');
+    },
+    onEdit (item) {
+      this.$emit('editForm', item);
     }
   }
 };
--- pages/roles/index.vue
+++ pages/roles/index.vue
@@ -1,8 +1,53 @@
 <template>
   <v-row>
     <v-col>
-      <v-btn @click="onCreate">New Form</v-btn>
       <RoleForm />
+      <client-only>
+        <v-data-table
+          :headers="headers"
+          :items="roles"
+          :search="search"
+        >
+          <template v-slot:top>
+            <v-toolbar
+              flat
+            >
+              <v-toolbar-title>Roles List</v-toolbar-title>
+              <v-spacer />
+              <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Search in Roles"
+                hide-details
+              />
+              <v-spacer />
+              <v-btn
+                color="primary"
+                dark
+                class="mb-4"
+                @click="onCreate"
+              >
+                New Role
+              </v-btn>
+            </v-toolbar>
+          </template>
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon
+
+              class="mr-2"
+              @click="onEdit(item)"
+            >
+              mdi-pencil
+            </v-icon>
+            <v-icon
+              color="warning"
+              @click="deleteItem(item)"
+            >
+              mdi-delete
+            </v-icon>
+          </template>
+        </v-data-table>
+      </client-only>
     </v-col>
   </v-row>
 </template>
@@ -12,9 +57,30 @@ export default {
   components: {
     RoleForm
   },
+  async asyncData ({ $content, params }) {
+    const roles = await $content('roles', params.slug)
+      .sortBy('createdAt', 'desc')
+      .fetch();
+
+    return { roles };
+  },
+  data: () => {
+    return {
+      search: '',
+      headers: [
+        { text: 'Name', value: 'name_mm' },
+        { text: 'Name_En', value: 'name_en' },
+        { text: 'Description', value: 'description' },
+        { text: 'Actions', value: 'actions', sortable: false }
+      ]
+    };
+  },
   methods: {
     onCreate () {
       this.$emit('createForm');
+    },
+    onEdit (item) {
+      this.$emit('editForm', item);
     }
   }
 };
