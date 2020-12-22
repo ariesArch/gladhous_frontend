5c333a750f44f35e3b2a4ec62dbb1cc399dab92d
SuMonAye216827 <sumon.sue@gmail.com>
2020-12-22 19:34:34 +0630

    setup tables

--- components/Forms/Table/TableForm.vue
+++ components/Forms/Table/TableForm.vue
@@ -1,42 +1,164 @@
 <template>
   <v-dialog v-model="isOpenDialog" max-width="600px">
     <v-card>
-      <v-card-title>Create New Table</v-card-title>
-      <v-form @submit.prevent="SaveForm">
-        <v-card-text>
-          <v-text-field
-            v-model="table_name"
-            v-validate="'required'"
-            label="Table Name"
-            name="table_name"
-          />
-          <v-text-field
-            v-model="table_no"
-            v-validate="'required'"
-            label="Table Number"
-            name="table_no"
-          />
-          <v-text-field
-            v-model="capacity"
-            v-validate="'required'"
-            label="Max Capacity"
-            name="capacity"
-            type="number"
-          />
-          <v-autocomplete
-            v-model="waiter"
-            v-validate="'required'"
-            :items="items"
-            label="Waiter"
-            name="waiter"
-          />
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="warning">Cancel</v-btn>
-          <v-btn color="info" @click="SaveForm()">Save</v-btn>
-        </v-card-actions>
-      </v-form>
+      <v-card-title>{{ dialogTitle }}</v-card-title>
+      <validation-observer ref="observer" v-slot="{ invalid }">
+        <v-form @submit.prevent="SaveForm">
+          <v-card-text class="mt-0 bg-info">
+            <v-row>
+              <v-col>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="table_name"
+                  rules="required"
+                >
+                  <v-text-field
+                    v-model="table.name"
+                    :error-messages="errors"
+                    required
+                    label="Table Name"
+                    name="table_name"
+                  />
+                </validation-provider>
+              </v-col>
+              <v-col>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="table_no"
+                  rules="required"
+                >
+                  <v-text-field
+                    v-model="table.table_no"
+                    :error-messages="errors"
+                    required
+                    label="Table Number"
+                    name="table_no"
+                  />
+                </validation-provider>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="capacity"
+                  rules="required"
+                >
+
+                  <v-text-field
+                    v-model="table.max_capacity"
+                    :error-messages="errors"
+                    required
+                    label="Max Capacity"
+                    name="capacity"
+                    type="number"
+                  />
+                </validation-provider>
+              </v-col>
+              <v-col>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="staff_id"
+                  rules="required"
+                >
+                  <v-autocomplete
+                    v-model="table.staff_id"
+                    :items="staffs"
+                    item-text="name"
+                    item-value="id"
+                    label="Choose Waiter"
+                    name="staff_id"
+                    :error-messages="errors"
+                    required
+                  />
+                </validation-provider>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="branch_id"
+                  rules="required"
+                >
+                  <v-autocomplete
+                    v-model="table.branch_id"
+                    :items="branches"
+                    item-text="name_mm"
+                    item-value="id"
+                    label="Choose Branch"
+                    name="branch_id"
+                    :error-messages="errors"
+                    required
+                  />
+                </validation-provider>
+              </v-col>
+              <v-col>
+
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="city_id"
+                  rules="required"
+                >
+                  <v-autocomplete
+                    v-model="table.city_id"
+                    :items="cities"
+                    item-text="name_mm"
+                    item-value="id"
+                    label="Choose City"
+                    name="city_id"
+                    :error-messages="errors"
+                    required
+                  />
+                </validation-provider>
+              </v-col>
+            </v-row>
+
+            <validation-provider
+              v-slot="{ errors }"
+              name="zone_id"
+              rules="required"
+            >
+              <v-autocomplete
+                v-model="table.zone_id"
+                :items="zones"
+                item-text="name_mm"
+                item-value="id"
+                label="Choose Zone"
+                name="zone_id"
+                :error-messages="errors"
+                required
+              />
+            </validation-provider>
+
+            <validation-provider
+              v-slot="{ errors }"
+              name="description"
+              rules="required"
+            >
+              <v-textarea
+                v-model="table.description"
+                :error-messages="errors"
+                label="Description"
+                name="description"
+                flat
+                height="30"
+                outlined
+                rows="3"
+                shaped
+                required
+              />
+            </validation-provider>
+
+          </v-card-text>
+          <v-card-actions class="mt-0">
+            <v-spacer />
+            <v-btn color="warning" @click="isOpenDialog=false">Cancel</v-btn>
+            <v-btn color="info" :disabled="invalid" @click="SaveForm()">Save</v-btn>
+          </v-card-actions>
+        </v-form>
+      </validation-observer>
+
     </v-card>
   </v-dialog>
 </template>
@@ -45,13 +167,34 @@ export default {
   data () {
     return {
       isOpenDialog: false,
-      table_name: '',
-      table_no: '',
-      capacity: ''
+      dialogTitle: 'Create new Table',
+      table: {
+        name: '',
+        table_no: '',
+        max_capacity: '',
+        staff_id: '',
+        branch_id: '',
+        city_id: '',
+        zone_id: '',
+        description: ''
+      },
+      staffs: [],
+      branches: [],
+      cities: [],
+      zones: []
     };
   },
   mounted () {
+    this.staffs = this.$parent.staffs;
+    this.branches = this.$parent.branches;
+    this.cities = this.$parent.cities;
+    this.zones = this.$parent.zones;
     this.$parent.$on('createForm', () => {
+      this.isOpenDialog = true;
+    });
+    this.$parent.$on('editForm', (item) => {
+      this.table = item;
+      this.dialogTitle = `Edit Table (${item.name})`;
       this.isOpenDialog = true;
     });
   },
--- pages/tables/index.vue
+++ pages/tables/index.vue
@@ -1,8 +1,32 @@
 <template>
   <v-row>
     <v-col>
-      <v-btn @click="onCreate">New Table</v-btn>
       <TableForm />
+      <client-only>
+        <v-data-table :headers="headers"
+                      :items="tables"
+                      :search="search"
+        >
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>Tables List</v-toolbar-title>
+              <v-spacer />
+              <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Search in Tables"
+                hide-details
+              />
+              <v-spacer />
+              <v-btn color="primary" dark class="mb-4" @click="onCreate">New Table</v-btn>
+            </v-toolbar>
+          </template>
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon color="info" @click="onEdit(item)">mdi-pencil</v-icon>
+            <v-icon color="red" class="mr-4" @click="onDelete(item)">mdi-delete</v-icon>
+          </template>
+        </v-data-table>
+      </client-only>
     </v-col>
   </v-row>
 </template>
@@ -12,9 +36,47 @@ export default {
   components: {
     TableForm
   },
+  async asyncData ({ $content }) {
+    const tables = await $content('tables')
+      .sortBy('name_mm', 'asc')
+      .fetch();
+    const cities = await $content('cities')
+      .sortBy('name_mm', 'asc')
+      .fetch();
+    const zones = await $content('zones')
+      .sortBy('name_mm', 'asc')
+      .fetch();
+    const branches = await $content('branches')
+      .sortBy('name_mm', 'asc')
+      .fetch();
+    const staffs = await $content('staffs')
+      .sortBy('name_mm', 'asc')
+      .fetch();
+
+    return { tables, branches, cities, zones, staffs };
+  },
+  data: () => {
+    return {
+      search: '',
+      headers: [
+        { text: 'Name', value: 'name' },
+        { text: 'Number', value: 'table_no' },
+        { text: 'Max_Capacity', value: 'max_capacity' },
+        { text: 'Waiter', value: 'staff.staff_name' },
+        { text: 'Branch', value: 'branch.branch_name' },
+        { text: 'City', value: 'city.city_name' },
+        { text: 'Zone', value: 'zone.zone_name' },
+        { text: 'Description', value: 'description' },
+        { text: 'Actions', value: 'actions' }
+      ]
+    };
+  },
   methods: {
     onCreate () {
       this.$emit('createForm');
+    },
+    onEdit (item) {
+      this.$emit('editForm', item);
     }
   }
 };
