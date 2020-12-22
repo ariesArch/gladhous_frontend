3cbe69e607bf931ab81c388ca796c74905e02605
Tun Lin Aung <htunlinaung153251@gmail.com>
2020-12-22 19:32:59 +0630

    update staff

--- pages/staffs/index.vue
+++ pages/staffs/index.vue
@@ -1,9 +1,29 @@
-
 <template>
   <v-row>
     <v-col>
-      <v-btn @click="onCreate">New Staff</v-btn>
       <StaffForm />
+      <client-only>
+        <v-data-table :headers="headers" :items="staffs" :search="search">
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>Staff List</v-toolbar-title>
+              <v-spacer />
+              <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Search in staffs"
+                hide-details
+              />
+              <v-spacer />
+              <v-btn color="primary" dark class="mb-4" @click="onCreate">New Staff</v-btn>
+            </v-toolbar>
+          </template>
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon color="info" @click="onEdit(item)">mdi-pencil</v-icon>
+            <v-icon color="red" class="mr-4" @click="onDelete(item)">mdi-delete</v-icon>
+          </template>
+        </v-data-table>
+      </client-only>
     </v-col>
   </v-row>
 </template>
@@ -13,9 +33,48 @@ export default {
   components: {
     StaffForm
   },
+  async asyncData ({ $content }) {
+    const cities = await $content('cities')
+      .sortBy('name_mm', 'asc')
+      .fetch();
+    const zones = await $content('zones')
+      .sortBy('name_mm', 'asc')
+      .fetch();
+    const departments = await $content('departments')
+      .sortBy('name_mm', 'asc')
+      .fetch();
+    const roles = await $content('roles')
+      .sortBy('name_mm', 'asc')
+      .fetch();
+    const branches = await $content('branches')
+      .sortBy('name_mm', 'asc')
+      .fetch();
+    const staffs = await $content('staffs')
+      .sortBy('name_mm', 'asc')
+      .fetch();
+    return { staffs, cities, zones, departments, roles, branches };
+  },
+  data: () => {
+    return {
+      search: '',
+      headers: [
+        { text: 'Name', value: 'name' },
+        { text: 'User name', value: 'username' },
+        { text: 'City Name', value: 'city.city_name' },
+        { text: 'Zone Name', value: 'zone.zone_name' },
+        { text: 'Branch Name', value: 'branch.branch_name' },
+        { text: 'Department Name', value: 'department.department_name' },
+        { text: 'Role Name', value: 'role.role_name' },
+        { text: 'Actions', value: 'actions' }
+      ]
+    };
+  },
   methods: {
     onCreate () {
       this.$emit('createForm');
+    },
+    onEdit (item) {
+      this.$emit('editForm', item);
     }
   }
 };
